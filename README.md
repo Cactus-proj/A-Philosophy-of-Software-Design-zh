c07c9eeb2d9e91846712cac64628ea1b0cf2023f
gdut-yy <gdut_yy@163.com>
2020-03-07 20:37:17 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# A-Philosophy-of-Software-Design-zh
+
+《软件设计的哲学》中文翻译
+
+在线阅读：[http://gdut_yy.gitee.io/doc-aposd/](http://gdut_yy.gitee.io/doc-aposd/)
+
+## Index
+
+todo
+
+## 本地开发 & 阅读
+
+本项目基于 vuepress 进行开发，以提供比 github mardown 更佳的阅读体验
+
+依赖于 `node.js`、`yarn`、`vuepress` 等环境
+
+```sh
+# vuepress
+yarn global add vuepress
+
+# 本地开发
+git clone https://github.com/gdut-yy/A-Philosophy-of-Software-Design-zh.git
+cd A-Philosophy-of-Software-Design-zh/
+yarn docs:dev
+```
+
+## License
+
+[MIT](https://github.com/gdut-yy/A-Philosophy-of-Software-Design-zh/blob/master/LICENSE)
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,42 @@
+// .vuepress/config.js
+module.exports = {
+  title: "《软件设计的哲学》中文翻译",
+  base: "/doc-aposd/",
+  themeConfig: {
+    repo: "gdut-yy/A-Philosophy-of-Software-Design-zh",
+    repoLabel: "Github",
+    docsRepo: "gdut-yy/A-Philosophy-of-Software-Design-zh",
+    docsBranch: "master/docs",
+    editLinks: true,
+    editLinkText: "帮助我们改善此页面！",
+    lastUpdated: "Last Updated",
+    sidebarDepth: 2,
+    nav: [],
+    sidebar: {
+      "/": [
+        "",
+        "ch1.md",
+        "ch2.md",
+        "ch3.md",
+        "ch4.md",
+        "ch5.md",
+        "ch6.md",
+        "ch7.md",
+        "ch8.md",
+        "ch9.md",
+        "ch10.md",
+        "ch11.md",
+        "ch12.md",
+        "ch13.md",
+        "ch14.md",
+        "ch15.md",
+        "ch16.md",
+        "ch17.md",
+        "ch18.md",
+        "ch19.md",
+        "ch20.md",
+        "ch21.md"
+      ]
+    }
+  }
+};
