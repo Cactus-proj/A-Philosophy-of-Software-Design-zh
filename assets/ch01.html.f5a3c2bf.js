868b138b03ae58be3ec4415535a2a2a3d0957efd
inkydragon <git@wo-class.cn>
2022-04-10 14:09:47 +0000

    Deploying to gh-pages from @ Cactus-proj/A-Philosophy-of-Software-Design-zh@ccc2a6e6b8af523d9c8b60ebe80ce2557a781dc0 🚀

---
+++ assets/ch01.html.f5a3c2bf.js
@@ -0,0 +1,1 @@
+const e={key:"v-67e9d9f4",path:"/en/ch01.html",title:"Chapter 1 Introduction",lang:"en-US",frontmatter:{},excerpt:"",headers:[{level:2,title:"1.1 How to use this book",slug:"_1-1-how-to-use-this-book",children:[]}],git:{updatedTime:1649599657e3},filePathRelative:"en/ch01.md"};export{e as data};
